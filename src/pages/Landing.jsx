3fbdf83acf01cc5847dab003a60fd98426dea746
Admin <admin@example.com>
2026-04-16 21:44:41 +0800

    implemented a minor fix

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export const Landing = () => {
-    const [menuOpen, setMenuOpen] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(true);
     return (
         <React.Fragment className="relative">
 
