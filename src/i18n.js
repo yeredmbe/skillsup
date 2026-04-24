57ce15c0a870b058531a9d73519ee789a18c4a5b
Admin <admin@example.com>
2026-04-24 23:41:37 +0800

    setting up clerk

--- src/i18n.js
+++ src/i18n.js
@@ -14,7 +14,7 @@ i18n
       escapeValue: false, // not needed for react as it escapes by default
     },
     backend: {
-      loadPath: '/locales/{{lng}}/{{ns}}.json',
+      loadPath: 'public/locales/{{lng}}/{{ns}}.json',
     }
   });
 
